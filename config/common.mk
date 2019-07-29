81dc54d40c9da93ada81505e4f74437358cd923e
Genkzsz11 <genkzsz11@gmail.com>
2019-07-29 20:24:47 +0700

    Some Stuff from aosip
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -53,6 +53,17 @@ PRODUCT_COPY_FILES += \
 endif
 endif
 
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Don't include art debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc
